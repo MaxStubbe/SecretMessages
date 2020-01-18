67b4b4bf86664c1841ef793bd70296812a5a215b
ROBIN-PC\robin <rm.hobbel@telfort.nl>
2020-01-18 14:52:52 +0100

    We currently just skip characters from utf 8

--- SecretMessages/SecretMessages.cpp
+++ SecretMessages/SecretMessages.cpp
@@ -27,7 +27,7 @@ int main()
 		//fh.Write_WAV("file-example_1mb.wav","file-example_1mb-OUT.wav" ,  "Dit is een testbericht");
 		//fh.Read_WAV("file-example_1mb-OUT.wav");
 		//fh.Read_WAV_optimized("lounge1.wav");
-		fh.Write_AIFF("base-16.aiff","base-16-out.aif", "Hällo");
+		//fh.Write_AIFF("base-16.aiff","base-16-out.aif", "Hällo");
 		fh.Read_AIFF("base-16-out.aif");
 		//fh.Read_AIFF("lounge1.aif");
 		//fh.Read_AIFF("infinitely-many-numbers.aif");
